6429052175dc7f1de30d5d409aee773b6ae436c9
Kevin Coyle <kevin.coyle@drutek.co.uk>
2017-04-23 20:38:11 +0100

    Added recent updates panel to homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,18 +2,44 @@
 @section('content')
     <div class="page-home">
         @include('partials.header')
-        <main class="is-hero">
-            <div class="container">
-                <div class="columns">
-                    <div class="column">
-                        <article>
-                            <h1 class="title has-text-centered">I'm a freelance web developer and occasional tech writer/blogger.</h1>
-                            <h2 class="subtitle has-text-centered">Welcome to my website.</h2>
-                        </article>
+        <main>
+            <section class="is-hero">
+                <div class="container">
+                    <div class="columns">
+                        <div class="column">
+                            <article>
+                                <h1 class="title has-text-centered">I'm a freelance web developer and occasional tech writer/blogger.</h1>
+                                <h2 class="subtitle has-text-centered">Welcome to my website.</h2>
+                            </article>
+                        </div>
                     </div>
                 </div>
-            </div>
-
+            </section>
+            <section class="section">
+                <div class="container">
+                    <div class="columns">
+                        <div class="column">
+                            <div class="section recent-updates">
+                                <h1 class="title">Recent Updates</h1>
+                                <ul class="columns">
+                                    @for ($i = 0; $i < 3; $i++)
+                                        <li class="column">
+                                            <article class="recent-updates__blog_item">
+                                                <header>
+                                                    <h1 class="title is-4">Blog Item</h1>
+                                                </header>
+                                                <div class="content">
+                                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet aperiam aspernatur consectetur, culpa, dignissimos dolor eius facere iste, laborum nam non numquam optio quidem recusandae rerum tempora voluptates voluptatibus.</p>
+                                                </div>
+                                            </article>
+                                        </li>
+                                    @endfor
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     </div>
 @endsection
